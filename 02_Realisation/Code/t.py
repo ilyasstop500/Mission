1313525d34368d03ed8c45b837528a0c475b7278
ilyasstop500 <ilyassdpsmain@gmail.com>
2024-07-02 19:31:08 +0200

    minor mistake

--- 02_Realisation/Code/t.py
+++ 02_Realisation/Code/t.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.run(["python3", "Code/01-ImportCsvBdd.py"])
-subprocess.run(["python3", "Code/02-Remplir_Refsql.py"])
-subprocess.run(["python3", "Code/03-Remplir_Result_Source.py"])
-subprocess.run(["python3", "Code/04-Remplir_Result_Calcul.py"])
+subprocess.run(["python3", r"Mission\02_Realisation\Code\01-ImportCsvBdd.py"])
+subprocess.run(["python3", r"Mission\02_Realisation\Code/02-Remplir_Refsql.py"])
+subprocess.run(["python3", r"Mission\02_Realisation\Code/03-Remplir_Result_Source.py"])
+subprocess.run(["python3", r"Mission\02_Realisation\Code/04-Remplir_Result_Calcul.py"])
